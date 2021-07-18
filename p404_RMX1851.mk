a934bb58a40536d7baf92fbe6669d9580ed1101c
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-07-18 13:05:13 +0530

    RMX1851: P404 bringup

--- p404_RMX1851.mk
+++ p404_RMX1851.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/404/configs/common.mk)
 
 # Inherit from RMX1851 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -18,7 +18,7 @@ BLASTER_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1851
-PRODUCT_NAME := aosp_RMX1851
+PRODUCT_NAME := p404_RMX1851
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 3 Pro
 PRODUCT_MANUFACTURER := Realme
